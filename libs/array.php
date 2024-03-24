83755b5b7e4a00b6661ab13987a84725835b402b
Parsa Mostafaie <155385612+parsa-mostafaie@users.noreply.github.com>
2024-03-24 13:05:57 +0330

    Create array.php

---
+++ libs/array.php
@@ -0,0 +1,19 @@
+<?php function array_any(array $array, callable $fn)
+{
+  foreach ($array as $value) {
+    if ($fn($value)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function array_every(array $array, callable $fn)
+{
+  foreach ($array as $value) {
+    if (!$fn($value)) {
+      return false;
+    }
+  }
+  return true;
+}
