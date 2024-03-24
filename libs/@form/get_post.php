56a56ff411cb55f78acb8624c0276944aa6e4469
Parsa Mostafaie <155385612+parsa-mostafaie@users.noreply.github.com>
2024-03-24 12:59:56 +0330

    Create get_post.php

---
+++ libs/@form/get_post.php
@@ -0,0 +1,18 @@
+<?php
+//? Check Both Of POST and GET Methods
+
+function get_val($name)
+{
+  $res = '';
+  if (isset ($_POST[$name])) {
+    $res = $_POST[$name];
+  } elseif (isset ($_GET[$name])) {
+    $res = $_GET[$name];
+  }
+  return htmlspecialchars($res);
+}
+
+function setted($name)
+{
+  return isset ($_POST[$name]) || isset ($_GET[$name]);
+}
