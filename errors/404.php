46b239ec9d73551928b572c8442500f41f4d61c4
Parsa Mostafaie <155385612+parsa-mostafaie@users.noreply.github.com>
2024-04-10 13:24:53 +0330

    404 Error Handling

---
+++ errors/404.php
@@ -0,0 +1,25 @@
+<!-- <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css"> -->
+<link rel="stylesheet" href="/assets/styles/style.css">
+<style>
+    body {
+        background: #dedede;
+    }
+
+    .page-wrap {
+        min-height: 100vh;
+    }
+</style>
+
+<div class="page-wrap d-flex flex-row align-items-center">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12 text-center">
+                <span class="display-1 d-block">404</span>
+                <div class="mb-4 lead">The page you are looking for was not found.</div>
+                <a href="/" class="btn btn-link">Back to Home</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+</html>
